4c40f3017f1f54b535feb49d22a6fff3e48ea8bd
marloes-stemerdink <marloes.stemerdink@radboudumc.nl>
2026-09-23 17:11:10 +0200

    Add files via upload

---
+++ input/finetuning_base_config.py
@@ -0,0 +1,383 @@
+checkpoint_file = None
+conv_kernel_size = 1
+crop_size = (
+    512,
+    512,
+)
+custom_imports = dict(allow_failed_imports=False, imports='mmpretrain.models')
+data_preprocessor = dict(
+    bgr_to_rgb=True,
+    mean=[
+        127.5,
+        127.5,
+        127.5,
+    ],
+    pad_val=0,
+    seg_pad_val=255,
+    size=(
+        512,
+        512,
+    ),
+    std=[
+        127.5,
+        127.5,
+        127.5,
+    ],
+    type='SegDataPreProcessor')
+data_root = '/mnt/data/dataset_training/subset_3/converted/'
+dataset_type = 'FSHD'
+default_hooks = dict(
+    checkpoint=dict(by_epoch=False, interval=300, type='CheckpointHook'),
+    logger=dict(interval=50, log_metric_by_epoch=False, type='LoggerHook'),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    timer=dict(type='IterTimerHook'),
+    visualization=dict(type='SegVisualizationHook'))
+default_scope = 'mmseg'
+env_cfg = dict(
+    cudnn_benchmark=True,
+    dist_cfg=dict(backend='nccl'),
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0))
+launcher = 'none'
+load_from = '/mnt/data/model_to_train/FSHD_config_SWIN_f0_binary.pth'
+log_level = 'INFO'
+log_processor = dict(by_epoch=False)
+model = dict(
+    auxiliary_head=dict(
+        align_corners=False,
+        channels=256,
+        concat_input=False,
+        dropout_ratio=0.1,
+        ignore_index=255,
+        in_channels=768,
+        in_index=2,
+        loss_decode=dict(
+            avg_non_ignore=True,
+            class_weight=[
+                0.69314718,
+                2.39377455,
+            ],
+            loss_weight=0.4,
+            type='CrossEntropyLoss',
+            use_sigmoid=False),
+        norm_cfg=dict(requires_grad=True, type='BN'),
+        num_classes=2,
+        num_convs=1,
+        type='FCNHead'),
+    backbone=dict(
+        attn_drop_rate=0.0,
+        depths=[
+            2,
+            2,
+            18,
+            2,
+        ],
+        drop_path_rate=0.3,
+        drop_rate=0.0,
+        embed_dims=192,
+        mlp_ratio=4,
+        num_heads=[
+            6,
+            12,
+            24,
+            48,
+        ],
+        out_indices=(
+            0,
+            1,
+            2,
+            3,
+        ),
+        patch_norm=True,
+        qk_scale=None,
+        qkv_bias=True,
+        type='SwinTransformer',
+        use_abs_pos_embed=False,
+        window_size=7),
+    data_preprocessor=dict(
+        bgr_to_rgb=True,
+        mean=[
+            127.5,
+            127.5,
+            127.5,
+        ],
+        pad_val=0,
+        seg_pad_val=255,
+        size=(
+            512,
+            512,
+        ),
+        std=[
+            127.5,
+            127.5,
+            127.5,
+        ],
+        type='SegDataPreProcessor'),
+    decode_head=dict(
+        kernel_generate_head=dict(
+            align_corners=False,
+            channels=512,
+            dropout_ratio=0.1,
+            ignore_index=255,
+            in_channels=[
+                192,
+                384,
+                768,
+                1536,
+            ],
+            in_index=[
+                0,
+                1,
+                2,
+                3,
+            ],
+            loss_decode=dict(
+            	avg_non_ignore=True,
+                class_weight=[
+                    0.69314718,
+                    2.39377455,
+                ],
+                loss_weight=1.0,
+                type='CrossEntropyLoss',
+                use_sigmoid=False),
+            norm_cfg=dict(requires_grad=True, type='BN'),
+            num_classes=2,
+            pool_scales=(
+                1,
+                2,
+                3,
+                6,
+            ),
+            type='UPerHead'),
+        kernel_update_head=[
+            dict(
+                conv_kernel_size=1,
+                dropout=0.0,
+                feat_transform_cfg=dict(
+                    act_cfg=None, conv_cfg=dict(type='Conv2d')),
+                feedforward_channels=2048,
+                ffn_act_cfg=dict(inplace=True, type='ReLU'),
+                in_channels=512,
+                kernel_updator_cfg=dict(
+                    act_cfg=dict(inplace=True, type='ReLU'),
+                    feat_channels=256,
+                    in_channels=256,
+                    norm_cfg=dict(type='LN'),
+                    out_channels=256,
+                    type='KernelUpdator'),
+                num_classes=2,
+                num_ffn_fcs=2,
+                num_heads=8,
+                num_mask_fcs=1,
+                out_channels=512,
+                type='KernelUpdateHead',
+                with_ffn=True),
+            dict(
+                conv_kernel_size=1,
+                dropout=0.0,
+                feat_transform_cfg=dict(
+                    act_cfg=None, conv_cfg=dict(type='Conv2d')),
+                feedforward_channels=2048,
+                ffn_act_cfg=dict(inplace=True, type='ReLU'),
+                in_channels=512,
+                kernel_updator_cfg=dict(
+                    act_cfg=dict(inplace=True, type='ReLU'),
+                    feat_channels=256,
+                    in_channels=256,
+                    norm_cfg=dict(type='LN'),
+                    out_channels=256,
+                    type='KernelUpdator'),
+                num_classes=2,
+                num_ffn_fcs=2,
+                num_heads=8,
+                num_mask_fcs=1,
+                out_channels=512,
+                type='KernelUpdateHead',
+                with_ffn=True),
+            dict(
+                conv_kernel_size=1,
+                dropout=0.0,
+                feat_transform_cfg=dict(
+                    act_cfg=None, conv_cfg=dict(type='Conv2d')),
+                feedforward_channels=2048,
+                ffn_act_cfg=dict(inplace=True, type='ReLU'),
+                in_channels=512,
+                kernel_updator_cfg=dict(
+                    act_cfg=dict(inplace=True, type='ReLU'),
+                    feat_channels=256,
+                    in_channels=256,
+                    norm_cfg=dict(type='LN'),
+                    out_channels=256,
+                    type='KernelUpdator'),
+                num_classes=2,
+                num_ffn_fcs=2,
+                num_heads=8,
+                num_mask_fcs=1,
+                out_channels=512,
+                type='KernelUpdateHead',
+                with_ffn=True),
+        ],
+        num_stages=3,
+        type='IterativeDecodeHead'),
+    pretrained= None,
+    test_cfg=dict(mode='whole'),
+    train_cfg=dict(),
+    type='EncoderDecoder')
+norm_cfg = dict(requires_grad=True, type='BN')
+num_stages = 3
+optim_wrapper = dict(
+    clip_grad=dict(max_norm=1, norm_type=2),
+    optimizer=dict(
+        betas=(
+            0.9,
+            0.999,
+        ), lr=6e-05, type='AdamW', weight_decay=0.0005),
+    paramwise_cfg=dict(
+        custom_keys=dict(
+            absolute_pos_embed=dict(decay_mult=0.0),
+            norm=dict(decay_mult=0.0),
+            relative_position_bias_table=dict(decay_mult=0.0))),
+    type='OptimWrapper')
+param_scheduler = [
+    dict(
+        begin=0, by_epoch=False, end=1000, start_factor=0.001,
+        type='LinearLR'),
+    dict(
+        begin=1000,
+        by_epoch=False,
+        end=80000,
+        milestones=[
+            60000,
+            72000,
+        ],
+        type='MultiStepLR'),
+]
+resume = False
+test_cfg = dict(type='TestLoop')
+test_dataloader = dict(
+    batch_size=8,
+    dataset=dict(
+        data_prefix=dict(img_path='images', seg_map_path='masks'),
+        data_root='/mnt/data/dataset_training/subset_1/together/',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(keep_ratio=True, scale=(
+                512,
+                512,
+            ), type='Resize'),
+            dict(reduce_zero_label=False, type='LoadAnnotations'),
+            dict(type='PackSegInputs'),
+        ],
+        type='FSHD'),
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(shuffle=False, type='DefaultSampler'))
+test_evaluator = dict(
+    iou_metrics=[
+        'mIoU',
+    ], type='IoUMetric')
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(keep_ratio=True, scale=(
+        512,
+        512,
+    ), type='Resize'),
+    dict(reduce_zero_label=False, type='LoadAnnotations'),
+    dict(type='PackSegInputs'),
+]
+train_cfg = dict(max_iters=3000, type='IterBasedTrainLoop', val_interval=300)
+train_dataloader = dict(
+    batch_size=2,
+    dataset=dict(
+        data_prefix=dict(
+            img_path='images', seg_map_path='masks'),
+        data_root='/mnt/data/dataset_training/subset_3/converted/training/',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(reduce_zero_label=False, type='LoadAnnotations'),
+            dict(keep_ratio=True, scale=(
+                512,
+                512,
+            ), type='Resize'),
+            dict(
+                cat_max_ratio=0.75, crop_size=(
+                    512,
+                    512,
+                ), type='RandomCrop'),
+            dict(prob=0.5, type='RandomFlip'),
+            dict(type='PhotoMetricDistortion'),
+            dict(type='PackSegInputs'),
+        ],
+        type='FSHD'),
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(shuffle=True, type='InfiniteSampler'))
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(reduce_zero_label=False, type='LoadAnnotations'),
+    dict(keep_ratio=True, scale=(
+        512,
+        512,
+    ), type='Resize'),
+    dict(cat_max_ratio=0.75, crop_size=(
+        512,
+        512,
+    ), type='RandomCrop'),
+    dict(prob=0.5, type='RandomFlip'),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='PackSegInputs'),
+]
+tta_model = dict(type='SegTTAModel')
+val_cfg = dict(type='ValLoop')
+val_dataloader = dict(
+    batch_size=8,
+    dataset=dict(
+        data_prefix=dict(img_path='images', seg_map_path='masks'),
+        data_root='/mnt/data/dataset_training/subset_3/converted/validation/',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(keep_ratio=True, scale=(
+                512,
+                512,
+            ), type='Resize'),
+            dict(reduce_zero_label=False, type='LoadAnnotations'),
+            dict(type='PackSegInputs'),
+        ],
+        type='FSHD'),
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(shuffle=False, type='DefaultSampler'))
+val_evaluator = dict(
+    iou_metrics=[
+        'mIoU',
+    ], type='IoUMetric')
+vis_backends = [
+    dict(type='LocalVisBackend'),
+    dict(type='WandbVisBackend'),
+]
+visualizer = dict(
+    classes=[
+        'background',
+        'muscle',
+    ],
+    dataset_name='FSHD_KNET_SWIN',
+    name='visualizer',
+    palette=[
+        (
+            0,
+            0,
+            0,
+        ),
+        (
+            128,
+            0,
+            128,
+        ),
+    ],
+    save_dir='/mnt/data/model_to_train/subset_3/',
+    type='SegLocalVisualizer',
+    vis_backends=[
+        dict(type='LocalVisBackend'),
+        dict(type='WandbVisBackend'),
+    ])
+work_dir = '/mnt/data/model_to_train/subset_3/'
